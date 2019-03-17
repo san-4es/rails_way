c805a58a710dfd6cf60fe9a01589e680ea455e38
Alexander <alexander.zaycev@mail.ru>
2019-03-17 23:07:55 +0300

    Action show and index in the RESTful controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,16 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render plain: "OK"
+    #render plain: "OK"
   end
 
   #/items/1 GET
   def show
+    unless @item = Item.where(id: params[:id]).first
+      #render "items/show"
+    #else
+      render plain: "Page not found", status: 404
+    end  
   end
   
   #/items/new GET
